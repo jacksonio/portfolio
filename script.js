fb07debc01480047ef6b7d3b19a9263363513a50
Jack <56225474+jacksonio@users.noreply.github.com>
2020-03-23 15:25:28 +0200

    list

---
+++ script.js
@@ -0,0 +1,33 @@
+
+let swiper = new Swiper('.swiper1', {
+    slidesPerView: 1,
+    slidesPerColumn: 2,
+    spaceBetween: 10,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        slidesPerColumn: 2,
+        spaceBetween: 20
+      },
+      1170: {
+        slidesPerView: 3,
+        slidesPerColumn: 2,
+        spaceBetween: 30
+      },
+    }
+  });
+
+let swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20
+      },
+      1170: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+    }
+  });
